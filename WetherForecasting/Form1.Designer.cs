c065f5c10c1a6a222b5ab92acd3d9b24e47c19c0
himo77 <56316743+himo77@users.noreply.github.com>
2020-09-29 18:01:47 -0500

    Add project files.

---
+++ WetherForecasting/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WetherForecasting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txtAirTemp = new System.Windows.Forms.TextBox();
+            this.txtWindChill = new System.Windows.Forms.TextBox();
+            this.txtSnowFall = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(366, 212);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Air Temperature";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(488, 402);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Snowfall";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(466, 304);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Wind Chill";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(488, 588);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 37);
+            this.lblStatus.TabIndex = 3;
+            // 
+            // txtAirTemp
+            // 
+            this.txtAirTemp.Location = new System.Drawing.Point(680, 210);
+            this.txtAirTemp.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtAirTemp.Name = "txtAirTemp";
+            this.txtAirTemp.Size = new System.Drawing.Size(388, 31);
+            this.txtAirTemp.TabIndex = 4;
+            this.txtAirTemp.Text = "0";
+            this.txtAirTemp.TextChanged += new System.EventHandler(this.txtAirTemp_TextChanged);
+            // 
+            // txtWindChill
+            // 
+            this.txtWindChill.Location = new System.Drawing.Point(680, 308);
+            this.txtWindChill.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtWindChill.Name = "txtWindChill";
+            this.txtWindChill.Size = new System.Drawing.Size(388, 31);
+            this.txtWindChill.TabIndex = 5;
+            this.txtWindChill.Text = "0";
+            this.txtWindChill.TextChanged += new System.EventHandler(this.txtWindChill_TextChanged);
+            // 
+            // txtSnowFall
+            // 
+            this.txtSnowFall.Location = new System.Drawing.Point(680, 406);
+            this.txtSnowFall.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtSnowFall.Name = "txtSnowFall";
+            this.txtSnowFall.Size = new System.Drawing.Size(388, 31);
+            this.txtSnowFall.TabIndex = 6;
+            this.txtSnowFall.Text = "0";
+            this.txtSnowFall.TextChanged += new System.EventHandler(this.txtSnowFall_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.txtSnowFall);
+            this.Controls.Add(this.txtWindChill);
+            this.Controls.Add(this.txtAirTemp);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Wether ForeCast";
+           
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txtAirTemp;
+        private System.Windows.Forms.TextBox txtWindChill;
+        private System.Windows.Forms.TextBox txtSnowFall;
+    }
+}
+
---
+++ WetherForecasting/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WetherForecasting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.ValidateWether();
+        }
+
+        private void txtAirTemp_TextChanged(object sender, EventArgs e)
+        {
+            this.ValidateWether();
+        }
+
+        private void txtWindChill_TextChanged(object sender, EventArgs e)
+        {
+            this.ValidateWether();
+        }
+
+        private void txtSnowFall_TextChanged(object sender, EventArgs e)
+        {
+            this.ValidateWether();
+        }
+
+        private void ValidateWether()
+        {
+            this.lblStatus.ForeColor = Color.Red;
+            double airTemp = 0;
+            if(!double.TryParse(this.txtAirTemp.Text, out airTemp) && !this.txtAirTemp.Text.Equals("-"))
+            {
+                this.lblStatus.Text = "Please enter a valid value for Air Temperature.";
+                return;
+            }
+                
+            double windChill = 0;
+            if (!double.TryParse(this.txtWindChill.Text, out windChill) && !this.txtWindChill.Text.Equals("-"))
+            {
+                this.lblStatus.Text = "Please enter a valid value for Wind Chill.";
+                return;
+            }
+
+            double snowFall = 0;
+            if (!double.TryParse(this.txtSnowFall.Text, out snowFall) && !this.txtSnowFall.Text.Equals("-"))
+            {
+                this.lblStatus.Text = "Please enter a valid value for Snow Fall.";
+                return;
+            }
+
+            if (airTemp <= -25 || windChill <= -35 || snowFall >= 6)
+            {
+                this.lblStatus.ForeColor = Color.Green;
+                this.lblStatus.Text = "School is likely to be cancelled.";
+            }
+            else
+            {
+                this.lblStatus.ForeColor = Color.Blue;
+                this.lblStatus.Text = "School is not likely to be cancelled.";
+            }
+        }
+
+        
+    }
+}
